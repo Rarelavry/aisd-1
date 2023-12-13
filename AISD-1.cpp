a8ace1d94391cca0eaddec0d78e0316b8831b4bc
Rarelavry <lavr.shvets@inbox.ru>
2023-12-13 16:23:33 +0400

    Добавьте файлы проекта.

---
+++ AISD-1.cpp
@@ -0,0 +1,79 @@
+﻿// AISD-1.cpp: определяет точку входа для приложения.
+//
+
+#include "AISD-1.h"
+
+using namespace std;
+
+
+
+complex<double> Vector<complex<double>>::operator*(const Vector<complex<double>>& rhs) const {
+	if (_dimension == rhs._dimension)
+	{
+		complex<float> sum(0.0, 0.0);
+		for (int i = 0; i < _dimension; i++) {
+			complex<double> z1 = _values[i];
+			complex<double> z2 = rhs[i];
+			complex<double> altered_z2(z2.real(), -1 * z2.imag());
+			sum += (z1 * altered_z2);
+		}
+		return sum;
+	}
+	else
+	{
+		throw invalid_argument("Vectors have diffrent dimensions");
+	}
+};
+
+
+template<typename T>
+ostream& operator<<(ostream& os, const Vector<T>& v) {
+	os << "( ";
+	int dimension = v.getDimension();
+	for (int i = 0; i < dimension; i++) {
+		os << v[i] << "; ";
+	}
+	return os << ")";
+}
+
+
+int main()
+{
+	cout << "My tests" << endl;
+	cout << "Vector scalar multiplication:" << endl;
+	complex<double> c1[3];
+	c1[0] = complex<double>(1.0, -3.0);
+	c1[1] = complex<double>(1.0, -1.0);
+	c1[2] = complex<double>(1.0, -3.0);
+	Vector<complex<double>> v1 = Vector<complex<double>>(3, c1);
+
+	complex<double> c2[3];
+	c2[0] = complex<double>(2.0, -2.0);
+	c2[1] = complex<double>(2.0, -2.0);
+	c2[2] = complex<double>(2.0, -2.0);
+	Vector<complex<double>> v2 = Vector<complex<double>>(3, c2);
+	 
+	complex<double> ans = (v1 * v2);
+	cout << v1 << endl;
+	cout << v2 << endl;
+	cout << ans << endl << endl;
+
+	cout << "Vector and scalar multiplication:" << endl;
+	Vector<int> v(3, -10, 10);
+	cout << v << endl;
+	v = 3 * v;
+	cout << v << endl;
+	v = v * 2;
+	cout << v << endl;
+
+
+	cout << endl << "Task 4" << endl;
+	Vector<int> a(2, -10, 10);
+	Vector<int> b(2, -10, 10);
+	while ( ( (a*b) / (a.vectorModule() * b.vectorModule()) )!=0) {
+		Vector<int> b2(2, -10, 10);
+		b = b2;
+	}
+	cout << a << endl << b << endl;
+	return 0;
+}
---
+++ AISD-1.h
@@ -0,0 +1,163 @@
+﻿// AISD-1.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include <iostream>
+#include <stdexcept>
+#include <random>
+#include <complex>
+
+template <typename T>
+class Vector {
+	int _dimension;
+	T* _values;
+public:
+	int getDimension() const{
+		return _dimension;
+	}
+
+	double vectorModule() const{
+		T sum = 0;
+		for (int i = 0; i < _dimension; i++) {
+			T x = _values[i];
+			sum += (x * x);
+		}
+		return sqrt(sum);
+	}
+
+	Vector(const Vector& v) {
+		_dimension = v._dimension;
+		_values = new T[_dimension];
+		for (int i = 0; i < _dimension; i++) {
+			_values[i] = v._values[i];
+		}
+	}
+
+	Vector& operator=(const Vector& v) {
+		for (int i = 0; i < _dimension; i++) {
+			_values[i] = v._values[i];
+		}
+		return *this;
+	}
+	
+	Vector() = delete;
+
+	Vector(int dimension) {
+		_dimension = dimension;
+		_values = new T[dimension];
+		for (int i = 0; i < dimension; i++) {
+			_values[i] = 0;
+		}
+	}
+
+	Vector(int dimension, T values[]) {
+		_dimension = dimension;
+		_values = new T[dimension];
+		for (int i = 0; i < dimension; i++) {
+			_values[i] = values[i];
+		}
+	}
+
+	Vector(int dimension, double minValue, double maxValue) { 
+		random_device rd;
+		mt19937 e{ rd() }; // or std::default_random_engine e{rd()};
+		uniform_real_distribution<double> dist{ minValue, maxValue };
+
+		_dimension = dimension;
+		_values = new T[dimension];
+		for (int i = 0; i < dimension; i++) {
+			_values[i] = dist(e);
+		}
+	}
+	
+	~Vector() {
+		delete[] _values;
+	}
+
+	T& operator[](int i) {
+		return _values[i];
+	}
+
+	const T& operator[](int i) const {
+		return _values[i];
+	}
+
+	Vector operator+(const Vector& rhs) const {
+		if (_dimension == rhs._dimension)
+		{
+			Vector v(_dimension);
+			for (int i = 0; i < _dimension; i++) {
+				v[i] = _values[i] + rhs[i];
+			}
+			return v;
+		}
+		else
+		{
+			throw invalid_argument("Vectors have diffrent dimensions");
+		}
+	}
+
+	Vector operator-(const Vector& rhs) const {
+		if (_dimension == rhs._dimension)
+		{
+			Vector v(_dimension);
+			for (int i = 0; i < _dimension; i++) {
+				v[i] = _values[i] - rhs[i];
+			}
+			return v;
+		}
+		else
+		{
+			throw invalid_argument("Vectors have diffrent dimensions");
+		}
+	}
+
+	T operator*(const Vector& rhs) const {
+		if (_dimension == rhs._dimension)
+		{
+			T sum = 0;
+			for (int i = 0; i < _dimension; i++) {
+				sum += _values[i] * rhs[i];
+			}
+			return sum;
+		}
+		else
+		{
+			throw invalid_argument("Vectors have diffrent dimensions");
+		}
+	}
+
+	Vector operator*(const T& rhs) const {
+		Vector v = Vector(*this);
+		for (int i = 0; i < _dimension; i++) {
+			v._values[i] *= rhs;
+		}
+		return v;
+	}
+
+	friend Vector operator*(const float& lhs, const Vector& rhs) {
+		Vector v = Vector(rhs);
+		for (int i = 0; i < v._dimension; i++) {
+			v._values[i] *= lhs;
+		}
+		return v;
+	}
+
+	Vector operator/(const T& rhs) const {
+		Vector v = Vector(this);
+		for (int i = 0; i < _dimension; i++) {
+			v._values[i] /= rhs;
+		}
+		return v;
+	}
+
+	bool operator==(const Vector& v) {
+		for (int i = 0; i < _dimension; i++) {
+			if (_values[i] != v._values[i])
+				return false;
+		}
+		return true;
+	}
+
+};
